4e70d7fbdecc87c3d74a6498a4eaf997802556c0
122045355-pixel <122045355@upq.edu.mx>
2026-05-20 04:08:26 -0600

    guardar datos en local storage

--- caja.js
+++ caja.js
@@ -1,29 +1,107 @@
-// js del lado del servidor 
+// OBTENER PEDIDOS GUARDADOS
+let pedidos =
+JSON.parse(localStorage.getItem("pedidos")) || [];
 
-// Array donde se guardarán los pedidos
-let pedidos = [];
 let totalVentas = 0;
-// Función para agregar pedidos
-function agregarPedido(cliente, producto, cantidad, precio) {
 
-    // Crear objeto del pedido
-    const pedido = {
-        cliente: cliente,
-        producto: producto,
-        cantidad: cantidad,
-        precio: precio,
-        total: cantidad * precio
+// MOSTRAR PEDIDOS AL ABRIR
+mostrarPedidos();
+
+function guardarPedido(){
+
+    let cliente =
+    document.getElementById("cliente").value;
+
+    let producto =
+    document.getElementById("producto").value;
+
+    let cantidad =
+    Number(document.getElementById("cantidad").value);
+
+    // VALIDACIONES
+    if(cliente === "" || producto === "" || cantidad <= 0){
+
+        alert("Completa todos los campos");
+        return;
+    }
+
+    agregarPedido(cliente, producto, cantidad);
+
+    // LIMPIAR INPUTS
+    document.getElementById("cliente").value = "";
+
+    document.getElementById("producto").value = "";
+
+    document.getElementById("cantidad").value = "";
+}
+
+
+// FUNCION PARA AGREGAR PEDIDOS
+function agregarPedido(cliente, producto, cantidad){
+
+    const precios = {
+        CafeG: 120,
+        CafeM: 90,
+        Quason: 80,
+        Papas: 50,
+        Refresco: 35
     };
 
-    // Guardar pedido en el array
+    const pedido = {
+
+        cliente: cliente,
+
+        producto: producto,
+
+        cantidad: cantidad,
+
+        precio: precios[producto],
+
+        total: cantidad * precios[producto]
+    };
+
+    // GUARDAR EN ARRAY
     pedidos.push(pedido);
 
-    // Actualizar el total de ventas
-    totalVentas += pedido.total;
+    // GUARDAR EN LOCALSTORAGE
+    localStorage.setItem(
+        "pedidos",
+        JSON.stringify(pedidos)
+    );
 
-    // Mostrar mensaje
-    console.log("Pedido agregado correctamente");
+    // ACTUALIZAR LISTA
+    mostrarPedidos();
 
-    // Mostrar pedido agregado
+    console.log("Pedido agregado");
     console.log(pedido);
+}
+
+
+// MOSTRAR PEDIDOS
+function mostrarPedidos(){
+
+    let lista =
+    document.getElementById("listaPedidos");
+
+    lista.innerHTML = "";
+
+    totalVentas = 0;
+
+    pedidos.forEach(pedido => {
+
+        let item =
+        document.createElement("li");
+
+        item.className = "list-group-item";
+
+        item.textContent =
+        `${pedido.cliente} - ${pedido.producto} - ${pedido.cantidad} x $${pedido.precio} = $${pedido.total}`;
+
+        lista.appendChild(item);
+
+        totalVentas += pedido.total;
+    });
+
+    document.getElementById("ventasTotales").textContent =
+    `Total ventas: $${totalVentas}`;
 }
